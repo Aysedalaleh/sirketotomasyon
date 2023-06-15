ad8ead790f99c901350899232ce5605685674654
Aysedalaleh <98123520+Aysedalaleh@users.noreply.github.com>
2023-06-15 07:02:19 -0700

    Update BitirmeContextModelSnapshot.cs

--- BitirmeContextModelSnapshot.cs
+++ BitirmeContextModelSnapshot.cs
@@ -5,6 +5,7 @@ using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
+
 namespace BitirmeProjesiWeb.Migrations
 {
     [DbContext(typeof(BitirmeContext))]
